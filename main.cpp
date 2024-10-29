f13d7e40d8536a467f0cd5196d90d27b8309b4e6
MatHyp <matijoker29@gmail.com>
2024-10-29 19:08:23 +0100

    token

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 using namespace std;
+
+enum class TokenTypes {
+    
+};
+
+class Tokenizer {
+public:
+    Tokenizer(string src) : _src(src) {};
+
+
+
+private:
+
+    string _src;
+};
+
 int main() {
     cout << "test";
     cout << "test";
     cout << "test";
+
+    Tokenizer token("( * ( + ( 2, 4))");
+
     return 0;
 }
